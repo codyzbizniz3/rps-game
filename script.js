c98dc2c0924c177d793dfda3ef9677db62c31eb2
codyzbizniz3 <codyzbizniz3@users.noreply.github.com>
2025-06-11 20:21:12 -0700

    Gets player choice, converts it to lower case and prints the cleaned input to the console.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+console.log('Welcome to Rock, Paper, Scissors!')
+
+let playerChoice = prompt('Please choose rock, paper, or scissors.');
+
+let cleanChoice = playerChoice.toLowerCase()
+
+
+console.log(`You chose ${cleanChoice}!`);
+
+
+
