a09282ca6bff16454955ff3e99143e859af56442
codyzbizniz3 <codyzbizniz3@users.noreply.github.com>
2025-06-12 10:37:50 -0700

    Adds getComputerChoice function

--- script.js
+++ script.js
@@ -2,10 +2,30 @@ console.log('Welcome to Rock, Paper, Scissors!')
 
 let playerChoice = prompt('Please choose rock, paper, or scissors.');
 
-let cleanChoice = playerChoice.toLowerCase()
-
+let cleanChoice = playerChoice.toLowerCase();
 
 console.log(`You chose ${cleanChoice}!`);
+
+function getComputerChoice() {
+    const number = Math.floor(Math.random() * 3) + 1;
+
+    let choice;
+    if (number === 1) {
+        choice = "rock";
+    } else if (number === 2) {
+        choice = "paper";
+    } else if (number === 3) {
+        choice = "scissors";
+    }
+
+    console.log(`Computer chooses ${choice}!`);
+}
+
+getComputerChoice();
+
+
+
+
 
 
 
