4512ee99b908c7a77a1bd54c0f0e7e016a715b88
codyzbizniz3 <codyzbizniz3@users.noreply.github.com>
2025-07-08 21:48:34 -0700

    Adds 'tie' display, changes getComputerChoice function to zero index, adds game results display, removes unnecessary JS.

--- script.js
+++ script.js
@@ -2,96 +2,61 @@ const computerScore = document.getElementById('comp-score');
 const playerScore = document.getElementById('player-score');
 const playerDisplay = document.getElementById('player-display');
 const compDisplay = document.getElementById('comp-display');
+const tieDisplay = document.getElementById('tie-game');
+const result = document.getElementById('result');
 const allChoices = document.querySelectorAll('button');
 let humanChoice;
 let computerChoice;
+let roundResult;
 
 allChoices.forEach(button => button.addEventListener('click', (e) => {
     humanChoice = e.target.textContent;
     playerDisplay.innerHTML = 'You chose ' + humanChoice + '!';
     getComputerChoice();
-
     compDisplay.innerHTML = 'Computer chose ' + computerChoice + '!';
+    playRound(humanChoice, computerChoice);
+    result.innerHTML = roundResult;
+
+    if (roundResult === "You win!") {
+        playerScore.innerHTML++;
+    } else if (roundResult === "Computer wins!") {
+        computerScore.innerHTML++;
+    } else if (roundResult === "It's a tie!") {
+        tieDisplay.innerHTML++;
+    }
 }));
 
 function getComputerChoice() {
-
-
-    const number = Math.floor(Math.random() * 3) + 1;
-    if (number === 1) {
+    const number = Math.floor(Math.random() * 3);
+    if (number === 0) {
         computerChoice = "Rock";
-    } else if (number === 2) {
+    } else if (number === 1) {
         computerChoice = "Paper";
-    } else if (number === 3) {
+    } else if (number === 2) {
         computerChoice = "Scissors";
-    }
-
-
+    };
     return computerChoice;
-}
-
-
-
-
-/*function getHumanChoice() {
-
-
-
-
-}
-
-
+};
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        return "tie"
+        roundResult = "It's a tie!";
     } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice == "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")
+        (humanChoice === "Rock" && computerChoice === "Scissors") ||
+        (humanChoice == "Paper" && computerChoice === "Rock") ||
+        (humanChoice === "Scissors" && computerChoice === "Paper")
     ) {
-        return "human";
+        roundResult = "You win!";
     } else {
-        return "computer";
-    }
-}
-
-function playGame() {
-
-
-
-
-
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    
-
-    const roundResult = playRound(humanSelection, computerSelection);
-
-    if (roundResult === "human") {
-        console.log(`You win this round! ${humanSelection} beats ${computerSelection}!`);
-        humanScore++;
-    } else if (roundResult === "computer") {
-        console.log(`You lose! ${computerSelection} beats ${humanSelection}!`);
-        computerScore++
-    } else {
-        console.log("It's a tie this round!");
+        roundResult = "Computer wins!";
     }
 
-    console.log(`Current Score: You ${humanScore} - Computer ${computerScore}`);
-}
+};
 
 
 
 
-if (humanScore > computerScore) {
-    console.log("You won the game!");
-} else if (computerScore > humanScore) {
-    console.log("The computer won the game!");
-} else {
-    console.log("The game is a tie!")
-}*/
+
 
 
 
